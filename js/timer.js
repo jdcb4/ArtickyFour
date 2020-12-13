cc939ffd43c1ddbef2e22fd5ca931fcb2c720edf
jdcb4 <joedore@gmail.com>
2020-12-13 19:02:34 +1100

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -4,11 +4,11 @@ function fnTimer() {
   var now = new Date().getTime();                             // Get today's date and time
   var distance = countDownDate - now;                         // Find the distance between now and the count down date
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);  // Time calculations for days, hours, minutes and seconds
-  document.getElementById("lblTurnTimer").innerHTML = "Time remaining: " + seconds + "s ";     // Display the result in the element with id="TurnTimer"
+  //document.getElementById("lblTurnTimer").innerHTML = "Time remaining: " + seconds + "s ";     // Display the result in the element with id="TurnTimer"
   // If the count down is finished, write some text
   if (distance < 0) {
       clearInterval(x);
-      document.getElementById("lblTurnTimer").innerHTML = "EXPIRED";
+      //document.getElementById("lblTurnTimer").innerHTML = "EXPIRED";
       fnEndTurn()
     }
   }, TimerUpdateInterval);                                    // Timer updated frequency, set from index.js
